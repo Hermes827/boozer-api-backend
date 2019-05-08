2b8113aeaf8e1f14cddcc818bc5442419edc987c
github username <github email address>
2019-05-08 16:42:23 -0700

    updated

--- app/controllers/api/v1/cocktails_controller.rb
+++ app/controllers/api/v1/cocktails_controller.rb
@@ -5,6 +5,32 @@ module Api
         render json: Cocktail.all
       end
 
+      def new
+        @cocktail = Cocktail.new
+      end
+
+      def create
+
+        proportions = cocktail_params[:proportions]
+        cocktail_params[:proportions] = nil
+        @cocktail = Cocktail.create(
+          name: cocktail_params[:name],
+          description: cocktail_params[:description],
+          instructions: cocktail_params[:instructions],
+          source: cocktail_params[:source],
+          proportions: []
+        )
+        ingredient = @cocktail.ingredients.create(name: proportions[0][:ingredient])
+        @cocktail.proportions.create(ingredient: ingredient, amount: proportions[0][:amount])
+
+
+        if @cocktail.save
+          render json: @cocktail, status: :accepted #here is the error, accepted
+        else
+          render json: { errors: @cocktail.errors.full_messages }, status: :unprocessible_entity
+        end
+      end
+
       def show
         cocktail = Cocktail.find(params[:id])
 
@@ -40,6 +66,13 @@ module Api
 
       def destroy
 
+      end
+
+      private
+
+      def cocktail_params
+        # params.permit(:title, :content)
+        params.require(:cocktail).permit!
       end
     end
   end
